59ea8c4a1f89ee14b45a93567f5f4ef30c0eb0c7
CuongPahm <cuongpahm1811@gmail.com>
2024-03-22 00:30:52 +0700

    Add project files.

---
+++ eStore/SessionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+public static class SessionExtensions
+{
+    //lay data tu session
+    public static T GetComplexData<T>(this ISession session, string key)
+    {
+        var data = session.GetString(key);
+        if (data == null)
+        {
+            return default(T);
+        }
+        return JsonConvert.DeserializeObject<T>(data);
+    }
+
+    // luu data vao session
+    public static void SetComplexData(this ISession session, string key, object value)
+    {
+        //chuyen doi value thanh json => luu session
+        session.SetString(key, JsonConvert.SerializeObject(value));
+    }
+}
+
